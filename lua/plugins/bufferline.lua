63496e99857e255cb4db371ce94f55db89818ee2
Luis Taniça <matallui@gmail.com>
2024-10-01 16:13:53 -0600

    fix(bufferline): offet when neo-tree is open

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,7 +3,18 @@ return {
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('bufferline').setup {}
+    require('bufferline').setup {
+      options = {
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Explorer',
+            highlight = 'PanelHeading',
+            padding = 1,
+          },
+        },
+      },
+    }
     vim.keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>')
     vim.keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>')
   end,
