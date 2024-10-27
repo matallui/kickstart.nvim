39e6ad7e049e379417da80e92e5bb7effd248466
Luis Taniça <matallui@gmail.com>
2024-10-28 06:30:47 +0700

    comment keymappings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -24,8 +24,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- More ergonomic Esc
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('i', 'jj', '<C-o>o')
 
+-- Save & Quit
 vim.keymap.set('n', '<leader>w', '<cmd>w!<cr>', { desc = 'Save' })
 vim.keymap.set('n', '<leader>q', '<cmd>confirm q<cr>', { desc = 'Quit' })
